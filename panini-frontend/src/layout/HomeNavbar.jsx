148d87dc73cac54b3cda8d677d99550160a47eda
saba-calin <sabaila.calin10@gmail.com>
2025-06-12 06:08:41 -0700

    added the home navbar

---
+++ panini-frontend/src/layout/HomeNavbar.jsx
@@ -0,0 +1,14 @@
+const HomeNavbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand">Panini</a>
+                <div>
+                    {/*<button>fsd</button>*/}
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default HomeNavbar;
---
+++ panini-frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import {Fragment} from "react";
+import HomeNavbar from "../layout/HomeNavbar.jsx";
+
+const Home = () => {
+    return (
+        <Fragment>
+            <HomeNavbar />
+
+
+        </Fragment>
+    );
+}
+
+export default Home;
